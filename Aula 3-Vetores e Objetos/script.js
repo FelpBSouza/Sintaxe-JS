3863ea6619a92fc98445af73ebc0d357c83d573e
FelpBSouza <110870428+FelpBSouza@users.noreply.github.com>
2023-02-09 11:52:47 -0300

    Aula 3-Vetores e Objetos

---
+++ Aula 3-Vetores e Objetos/script.js
@@ -0,0 +1,23 @@
+//declarar arrays
+let array = ['string',1,true];
+console.log(array);
+
+//guardar arrays
+let array1 = ['string',1,true,['array1']];
+console.log(array1);
+
+
+//arrau tipos
+forEach( ) = itera um array ;
+push() = add um item  no final do arrray ;
+pop() = remove um item  no final do array ;
+shift() = remove item no inicio do array ;
+unshift() = add item no inicio do array ;
+indexOf() = retorna o indice de um valor ;
+splice() = remove ou substitui um item  pelo indice ;
+slice() = retorna uma parte de um array existente ;
+
+//objeto
+let object = {string:'string',number:1,boolean:true,array:['array1'],objectInterno:{objectInterno:'object interno'}};
+
+console.log(object);
