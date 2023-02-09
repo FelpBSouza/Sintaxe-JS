9d523b7f65de73cbb379fd47790df222486d03a4
FelpBSouza <110870428+FelpBSouza@users.noreply.github.com>
2023-02-09 11:53:32 -0300

    Aula 5-Funções

---
+++ Aula 5-Funções/script.js
@@ -0,0 +1,29 @@
+function funcao(){
+    console.log('Tudo certo');
+}
+funcao();
+
+
+function mensagem(primeiro,segundo){
+    console.log(primeiro,segundo);
+}
+
+mensagem('Tudo certo','jovem');
+
+//função declarativa 
+function funcao(){
+    console.log('Tudo certo');
+}
+funcao();
+
+//expressão de função com nomeação
+ var funcao =function funcao(){// function(){}
+    console.log('Tudo certo');
+ }
+ funcao();
+
+ //arrow function
+ var funcao =()=>{
+    console.log('Arrow');
+ }
+ funcao();
